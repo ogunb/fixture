0c458af34acfdc5ab8d87775a2ff84576694acc0
Ogun Babacan <ogun.babacan@trendyol.com>
2023-08-04 16:21:40 +0300

    revalidate

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Calendar from "./teams/fixture/calendar";
 import Follow from "./teams/follow/follow";
 
+export const revalidate = 10;
+
 export default function Home() {
   return (
     <main className="p-5 flex items-center flex-col gap-5">
